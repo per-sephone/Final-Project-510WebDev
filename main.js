e79df96d84f0c3028af014161cb1f1a00b550afe
Nora Luna <86637985+per-sephone@users.noreply.github.com>
2022-08-03 22:54:01 -0700

    updated color theme button

--- main.js
+++ main.js
@@ -35,22 +35,47 @@ function handleSubmit(event) {
 }
 
 function handleClick(event) {
-  document.body.style.background = 'white';
-  document.body.style.color = 'black';
-  for (let i = 0; i < socialsLink.length; i++) {
-    socialsLink[i].style.backgroundColor = 'white';
-    socialsLink[i].style.color = 'black';
+  if (light.innerHTML === 'Light Theme') {
+    document.body.style.background = 'white';
+    document.body.style.color = 'black';
+    for (let i = 0; i < socialsLink.length; i++) {
+      socialsLink[i].style.backgroundColor = 'white';
+      socialsLink[i].style.color = 'black';
+    }
+    for (let i = 0; i < projectLink.length; i++) {
+      // projectLink[i].style.backgroundColor = 'rgb(223, 223, 223)';
+      projectLink[i].style.color = 'black';
+    }
+    for (let i = 0; i < navLink.length; i++) {
+      navLink[i].style.backgroundColor = 'rgb(223, 223, 223)';
+      navLink[i].style.color = 'black';
+    }
+    section4.style.backgroundColor = 'rgb(223, 223, 223)';
+    nav.style.backgroundColor = 'rgb(223, 223, 223)';
+    light.innerHTML = 'Dark Theme';
+    light.style.backgroundColor = 'black';
+    light.style.color = 'white';
+  } else {
+    document.body.style.background = '';
+    document.body.style.color = '';
+    for (let i = 0; i < socialsLink.length; i++) {
+      socialsLink[i].style.backgroundColor = '';
+      socialsLink[i].style.color = '';
+    }
+    for (let i = 0; i < projectLink.length; i++) {
+      projectLink[i].style.backgroundColor = '';
+      projectLink[i].style.color = '';
+    }
+    for (let i = 0; i < navLink.length; i++) {
+      navLink[i].style.backgroundColor = '';
+      navLink[i].style.color = '';
+    }
+    section4.style.backgroundColor = '';
+    nav.style.backgroundColor = '';
+    light.innerHTML = 'Light Theme';
+    light.style.backgroundColor = '';
+    light.style.color = '';
   }
-  for (let i = 0; i < projectLink.length; i++) {
-    // projectLink[i].style.backgroundColor = 'rgb(223, 223, 223)';
-    projectLink[i].style.color = 'black';
-  }
-  for (let i = 0; i < navLink.length; i++) {
-    navLink[i].style.backgroundColor = 'rgb(223, 223, 223)';
-    navLink[i].style.color = 'black';
-  }
-  section4.style.backgroundColor = 'rgb(223, 223, 223)';
-  nav.style.backgroundColor = 'rgb(223, 223, 223)';
 }
 
 // window.addEventListener('keyup', (event) => {
